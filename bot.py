da8705c0d141072e5addde6bed5a7bd1de7389e6
metiriazi <metiriazi@gmail.com>
2026-07-26 15:10:10 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,62 @@
+import asyncio
+import os
+
+from aiohttp import web
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-bot = Bot("8902169504:AAEaD7cB2edVyCOtB6auWHvKfKOX1t04cnA")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+WEBHOOK_PATH = "/webhook"
+
+bot = Bot(
+    token=BOT_TOKEN,
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
+
 dp = Dispatcher()
+
 
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer("سلام 🌹")
+    await message.answer("سلام 👋 ربات با Webhook اجرا شده است.")
 
-async def main():
-    await dp.start_polling(bot)
+
+async def on_startup(bot: Bot):
+    render_url = os.getenv("RENDER_EXTERNAL_URL")
+
+    if render_url:
+        await bot.set_webhook(f"{render_url}{WEBHOOK_PATH}")
+        print("Webhook Set!")
+
+
+async def on_shutdown(bot: Bot):
+    await bot.delete_webhook()
+
+
+def main():
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    app = web.Application()
+
+    SimpleRequestHandler(
+        dispatcher=dp,
+        bot=bot
+    ).register(app, path=WEBHOOK_PATH)
+
+    setup_application(app, dp, bot=bot)
+
+    web.run_app(
+        app,
+        host="0.0.0.0",
+        port=int(os.getenv("PORT", 10000))
+    )
+
 
 if __name__ == "__main__":
-    import asyncio
-    asyncio.run(main())
+    main()
